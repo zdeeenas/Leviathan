0e3ee165d378d57518ee24d4f34a6c258ad723e2
zdeeenas <zdeeenas@email.com>
2026-04-12 10:28:20 +0200

    Autocommit from 2026-04-12 10:28:20
    Klipper version: v2026.04.00-0-gab900a01
    
    Moonraker version: v0.10.0-19-g1ed102e
    
    Mainsail version: v2.17.0

--- autocommit_spoolman.sh
+++ autocommit_spoolman.sh
@@ -3,18 +3,30 @@ set -e
 
 # === Configuration ===
 DB_PATH="/home/pi/spoolman/data/spoolman.db"
+
+# ukládáme do stejného repo jako config
 REPO_DIR="/home/pi/printer_data/config"
-TARGET_DB="$REPO_DIR/spoolman.db"
+TARGET_DB="$REPO_DIR/spoolman-backup/spoolman.db"
 
 echo "=== BACKUP START ==="
 echo "Time: $(date)"
 
-# Copy DB
+# vytvoř složku pokud neexistuje
+mkdir -p "$REPO_DIR/spoolman-backup"
+
+# kontrola jestli DB existuje
+if [ ! -f "$DB_PATH" ]; then
+    echo "ERROR: Spoolman DB not found!"
+    exit 1
+fi
+
+# kopie DB
 cp -f "$DB_PATH" "$TARGET_DB"
 
-cd "$REPO_DIR" || exit
+cd "$REPO_DIR"
 
-/usr/bin/git add spoolman.db
+# git add jen soubor
+/usr/bin/git add spoolman-backup/spoolman.db
 
 if ! /usr/bin/git diff --cached --quiet; then
     /usr/bin/git commit -m "Auto backup Spoolman DB $(date '+%Y-%m-%d %H:%M:%S')"
